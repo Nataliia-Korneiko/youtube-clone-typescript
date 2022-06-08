4f3bfdeb4801a2b6be32475046f5c95c7d55a838
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2022-06-08 20:30:13 +0300

    add layout

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,54 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <NotificationsProvider>
+          {getLayout(
+            <main>
+              <Component {...pageProps} />
+            </main>
+          )}
+        </NotificationsProvider>
+      </MantineProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
